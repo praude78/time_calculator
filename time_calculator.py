7847d1d68bd28ca0ea3fe646d2f31cd2e3ed5164
Lucio Praude <praude78@gmail.com>
2023-07-10 12:10:15 +0100

    Construct the final output

--- time_calculator.py
+++ time_calculator.py
@@ -33,6 +33,24 @@ def add_time(start_time, duration, start_day=None):
         start_day_index = days_of_week.index(start_day)
         end_day_index = (start_day_index + days_later) % 7
         end_day = days_of_week[end_day_index]
+        
+    # Construct the final output
+    end_time = f"{end_time_hour}:{end_time_minute:02d} {end_time_period}"
+    if start_day:
+        if days_later == 0:
+            return f"{end_time}, {end_day}"
+        elif days_later == 1:
+            return f"{end_time}, {end_day} (next day)"
+        else:
+            return f"{end_time}, {end_day} ({days_later} days later)"
+    else:
+        if days_later == 0:
+            return end_time
+        elif days_later == 1:
+            return f"{end_time} (next day)"
+        else:
+            return f"{end_time} ({days_later} days later)"
+
 
 
 

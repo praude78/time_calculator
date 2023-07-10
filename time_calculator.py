b606078858601ff0e63bd853126d4d22682bd99e
Lucio Praude <praude78@gmail.com>
2023-07-10 12:01:28 +0100

    parse duration time

--- time_calculator.py
+++ time_calculator.py
@@ -2,3 +2,7 @@ def add_time(start_time, duration, start_day=None):
     # Parse the start time
     start_time_hour, start_time_minute = map(int, start_time.split(":"))
     start_time_period = start_time.split()[-1]  # AM or PM
+    
+    # Parse the duration time
+    duration_hour, duration_minute = map(int, duration.split(":"))
+
